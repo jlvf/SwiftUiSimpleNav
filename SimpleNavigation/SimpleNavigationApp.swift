63d57fa9259721e952a6ff96e62a2e33ea30cb4c
Joseph Lehr <dev@bitiverse.io>
2020-12-13 21:14:03 -0700

    Initial Commit

---
+++ SimpleNavigation/SimpleNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleNavigationApp.swift
+//  SimpleNavigation
+//
+//  Created by dev on 12/13/20.
+//
+
+import SwiftUI
+
+@main
+struct SimpleNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
